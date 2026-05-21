e34b597dc1de49431403086b5cdf0ad6ebed4ea7
Sumaiya2209 <sumaiyajannt2209@gmail.com>
2026-05-21 22:45:02 +0600

    update index file

--- index.js
+++ index.js
@@ -10,3 +10,22 @@ dotenv.config();
 const uri = process.env.MONGODB_URI;
 const PORT = process.env.PORT || 5000;
 const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:3000";
+
+const app = express();
+app.use(
+  cors({
+    origin: CLIENT_URL,
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
